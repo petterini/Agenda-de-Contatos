9103e85941d69816ecf011bfeac425f465f36e15
Pedro Petterini <ppetterini@gmail.com>
2024-11-18 14:53:47 -0300

    Adicionando um comentário interessante.

--- AgendaComTelas/src/TelaPrincipal.java
+++ AgendaComTelas/src/TelaPrincipal.java
@@ -32,6 +32,7 @@ public class TelaPrincipal extends JPanel {
 
 	private void chamaLista() {
 		arq.ler();
+		// comentário para forçar um conflito
 		DefaultTableModel m = (DefaultTableModel) table.getModel();
 		m.setRowCount(0);
 		for (Pessoa p : Principal.pessoas) {
